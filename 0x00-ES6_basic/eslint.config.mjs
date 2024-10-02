b94618535891eaf7f7bfc96fb0275a28b82f0cf7
Stephani6G <stephaniegloria283@gmail.com>
2024-10-01 20:24:25 -0700

    depedencies install

--- 0x00-ES6_basic/eslint.config.mjs
+++ 0x00-ES6_basic/eslint.config.mjs
@@ -1,12 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  ...tseslint.configs.recommended,
 ];
